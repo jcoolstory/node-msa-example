2cccd9234500f5052379b04e520b4e23bbba44cb
siwon <jeongsw@we-gokorea.com>
2019-03-19 22:02:02 +0900

    distributor added tcp server / client added

---
+++ distributor.js
@@ -0,0 +1,66 @@
+
+const server = require('./server.js');
+
+var map = {}
+
+class distributor extends server {
+    constructor() {
+        super("distributor", 9000, ["POST/distributes", "GET/ddistributes"]);
+    }
+
+    onCreate(socket) {
+        console.log("onCreate", socket.remoteAddress, socket.remotePort);
+        this.sendInfo(socket);
+    }
+
+    onClose(socket) {
+        var key = socket.remoteAddress + ":" + socket.remotePort;
+        console.log("onClose", socket.remoteAddress, socket.remotePort);
+
+        delete map[key];
+        this.sendInfo();
+    }
+
+    onRead(socket, json) {
+        var key = socket.remoteAddress + ":" + socket.remotePort;
+
+        console.log("onRead", socket.remoteAddress, socket.remotePort, json);
+
+        if (json.uri == "/distributes" && json.method == "POSt") {
+            map[key] = {
+                socket:socket
+            };
+
+            map[key].info = json.params;
+            map[key].info.host = socket.remoteAddress;
+            this.sendInfo();
+        }
+    }
+    write(socket, packet) {
+        socket.write(JSON.stringify(packet) + "\n");
+    }
+
+    sendInfo(socket) {
+
+        var packet = {
+            uri : "/distributes",
+            method : "GET",
+            key : 0,
+            params : []
+        };
+
+        for (var n in map) {
+            packet.params.push(map[n].info);
+        }
+
+        if (socket) {
+            this.write(socket, packet);
+        } else {
+            for (var n in map) {
+                this.write(map[n].socket, packet);
+            }
+        }
+    }
+}
+
+new distributor();
